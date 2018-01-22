572c3b51ea7d8fa35628d4ffa7428a403cb67fa3
Cristian Schuszter <darkgeeky@gmail.com>
2018-01-22 12:48:28 +0200

    More on the SOM implementation.

--- ids_utils.py
+++ ids_utils.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-import numpy as np 
+import numpy as np
 import pandas as pd
 
 IDS_COLUMNS = [
@@ -58,14 +58,14 @@ def df_to_tf_matrix(df):
     return df.as_matrix(result).astype(np.float64)
 
 
-def get_label_column(dataset_file):
+def get_label_column(dataset_file, col=41):
     return np.genfromtxt(dataset_file,
-         delimiter=',', usecols=(41), dtype=str)
+                         delimiter=',', usecols=(col), dtype=str)
 
 
 def load_ids_df(path):
     testdf = pd.read_csv(path)
-    testdf.dropna(inplace=True,axis=1)
+    testdf.dropna(inplace=True, axis=1)
     testdf.columns = IDS_COLUMNS
     return testdf
 
@@ -88,3 +88,29 @@ def map_winner_neurons(som, data, target_index=-1):
         else:
             target_dict[target] += 1
     return mappings_list
+
+# Dataframe conversion utils
+
+
+def append_label_column(data, df, col=41):
+    label_col = df.columns[col]
+    target = df[label_col]
+    reshaped_target = np.reshape(target, (len(target), 1))
+    return np.hstack((data, reshaped_target)).tolist()
+
+
+def encode_dummy_variables(df, name):
+    # ex: tcp, udp -> [1,0], [0,1]
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = "{}-{}".format(name, x)
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+    if sd is None:
+        sd = df[name].std()
+    df[name] = (df[name] - mean) / sd
